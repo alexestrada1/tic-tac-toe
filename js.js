3f02bd7742e1b2f89f915ddccc7a64c32f0546f0
alexestrada1 <estradaalex46@gmail.com>
2023-01-31 15:32:23 -0600

    Got the x and o to show up in the website now need to work on the game mechanics. Also got the reset button to work

--- js.js
+++ js.js
@@ -8,12 +8,18 @@ const container = document.getElementsByClassName("container")
         }
         const resetGameBoard = () => {
             gameboard = [['1','2','3'],['4','5','6'],['7','8','9']]
+            const gameCells = document.querySelectorAll('.gameCell')
+            gameCells.forEach((cell) =>{
+              cell.classList.remove('player1')
+              cell.classList.remove('player2')
+                });
         }
         const createGameBoard = () =>{
             for(let i=0; i<9; i++){
                 var squareDiv = document.createElement('div')
                 //Text content for testing, will remove later
                 squareDiv.textContent = i;
+                squareDiv.classList.add('gameCell')
                 container[0].appendChild(squareDiv)
             }
             
@@ -22,10 +28,20 @@ const container = document.getElementsByClassName("container")
     })();
 
     function createPlayer(name) {
+        var values = [];
+        const setPlayerValues = (value) =>{
+            values = value;
+        }
+        const getPlayerValues = () => {
+            return values
+        }
         return {
           name: name,
           getFullName() {
-            return name;
+            return name,
+            values,
+            setPlayerValues,
+            getPlayerValues;
           },
         };
       }
@@ -33,7 +49,24 @@ const container = document.getElementsByClassName("container")
         Gameboard.createGameBoard();
         const player1 = createPlayer('player1') 
         const player2 = createPlayer('player2')
-
+        let turn = 1;
+        const gameCells = document.querySelectorAll('.gameCell')
+        gameCells.forEach((cell) =>{
+            cell.addEventListener(('click'), function(){
+              if(turn % 2 == 0){
+                cell.classList.add("player2");
+              } 
+              else{
+                cell.classList.add('player1')
+              }
+              turn++;
+              console.log(turn)
+            })
+            });
+        console.log(gameCells)
     }
     const startButton = document.querySelector(".start")
     startButton.addEventListener('click', playGame)
+    const resetButton = document.querySelector('.clear')
+    resetButton.addEventListener('click', Gameboard.resetGameBoard)
+
