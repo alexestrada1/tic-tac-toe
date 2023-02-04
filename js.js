6d02be55b424e9b4cf7da7fd13a2056191d98ec2
alexestrada1 <estradaalex46@gmail.com>
2023-02-03 18:39:59 -0600

    got the symbols to not voerwrite each other, also got the gameboard array to match the display, all the is left to do is check for gameover

--- js.js
+++ js.js
@@ -1,72 +1,94 @@
-const container = document.getElementsByClassName("container")
+const container = document.getElementsByClassName("container");
 //Gets conainer div, uased to create board from js
-    const Gameboard  = (() => {
-        let gameboard = [['1','2','3'],['4','5','6'],['7','8','9']]
-        const getGameboard = () => {
-            console.log(gameboard);
-            return gameboard;
-        }
-        const resetGameBoard = () => {
-            gameboard = [['1','2','3'],['4','5','6'],['7','8','9']]
-            const gameCells = document.querySelectorAll('.gameCell')
-            gameCells.forEach((cell) =>{
-              cell.classList.remove('player1')
-              cell.classList.remove('player2')
-                });
-        }
-        const createGameBoard = () =>{
-            for(let i=0; i<9; i++){
-                var squareDiv = document.createElement('div')
-                //Text content for testing, will remove later
-                squareDiv.textContent = i;
-                squareDiv.classList.add('gameCell')
-                container[0].appendChild(squareDiv)
-            }
-            
-        }
-        return {getGameboard, resetGameBoard, createGameBoard}
-    })();
-
-    function createPlayer(name) {
-        var values = [];
-        const setPlayerValues = (value) =>{
-            values = value;
-        }
-        const getPlayerValues = () => {
-            return values
-        }
-        return {
-          name: name,
-          getFullName() {
-            return name,
-            values,
-            setPlayerValues,
-            getPlayerValues;
-          },
-        };
-      }
-    function playGame(){
-        Gameboard.createGameBoard();
-        const player1 = createPlayer('player1') 
-        const player2 = createPlayer('player2')
-        let turn = 1;
-        const gameCells = document.querySelectorAll('.gameCell')
-        gameCells.forEach((cell) =>{
-            cell.addEventListener(('click'), function(){
-              if(turn % 2 == 0){
-                cell.classList.add("player2");
-              } 
-              else{
-                cell.classList.add('player1')
-              }
-              turn++;
-              console.log(turn)
-            })
-            });
-        console.log(gameCells)
+const Gameboard = (() => {
+  let gameboard = [
+    ["0", "1", "2"],
+    ["3", "4", "5"],
+    ["6", "7", "8"],
+  ];
+  const getGameboard = () => {
+    console.log(gameboard);
+    return gameboard;
+  };
+  const resetGameBoard = () => {
+    gameboard = [
+      ["1", "2", "3"],
+      ["4", "5", "6"],
+      ["7", "8", "9"],
+    ];
+    const gameCells = document.querySelectorAll(".gameCell");
+    gameCells.forEach((cell) => {
+      cell.classList.remove("o");
+      cell.classList.remove("x");
+    });
+  };
+  const createGameBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      var squareDiv = document.createElement("div");
+      //Text content for testing, will remove later
+      squareDiv.textContent = i;
+      squareDiv.classList.add("gameCell");
+      container[0].appendChild(squareDiv);
     }
-    const startButton = document.querySelector(".start")
-    startButton.addEventListener('click', playGame)
-    const resetButton = document.querySelector('.clear')
-    resetButton.addEventListener('click', Gameboard.resetGameBoard)
+  };
+  const setGameboardVal = (row, col, className) => {
+    if (className == "o") {
+      console.log(gameboard[row][col])
+      gameboard[row][col] = "o";
+    } else {
+      gameboard[row][col] = "x";
+    }
+  };
+  return { getGameboard, resetGameBoard, createGameBoard, setGameboardVal};
+})();
 
+function createPlayer(name) {
+  var values = [];
+  const setPlayerValues = (value) => {
+    values.push(value);
+  };
+  const getPlayerValues = () => {
+    return values;
+  };
+  return {
+    name: name,
+    getFullName() {
+      return name, values, setPlayerValues, getPlayerValues;
+    },
+  };
+}
+function playGame() {
+  Gameboard.createGameBoard();
+  const o = createPlayer("o");
+  const x = createPlayer("x");
+  let turn = 1;
+  const gameCells = document.querySelectorAll(".gameCell");
+  gameCells.forEach((cell) => {
+    cell.addEventListener("click", function () {
+      if (cell.classList.contains("o") || cell.classList.contains("x")) {
+        //empty brackets to do nothing if a cell already has a class
+      } else {
+        let row;
+        let cellNum = parseInt(cell.textContent)
+        let col = cellNum % 3; //the modular gets the column
+        if(cellNum <= 2){row = 0;}
+        if(cellNum <= 5 && cellNum > 2){row = 1}
+        if(cellNum <= 8 && cellNum > 5){row = 2}
+        if (turn % 2 == 0) {
+          cell.classList.add("x");
+          Gameboard.setGameboardVal(row, col, 'x')
+        } else {
+          cell.classList.add("o");
+          Gameboard.setGameboardVal(row, col, 'o')
+        }
+        Gameboard.getGameboard();
+        turn++;
+      }
+    });
+  });
+  console.log(gameCells);
+}
+const startButton = document.querySelector(".start");
+startButton.addEventListener("click", playGame);
+const resetButton = document.querySelector(".clear");
+resetButton.addEventListener("click", Gameboard.resetGameBoard);
